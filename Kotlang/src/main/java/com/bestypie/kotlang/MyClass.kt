95781748258363b6f6220e9345cc34a75b52711a
Karumaidoi <mainahmwangi12@gmail.com>
2023-01-13 21:43:05 +0300

    first commit

---
+++ Kotlang/src/main/java/com/bestypie/kotlang/MyClass.kt
@@ -0,0 +1,17 @@
+package com.bestypie.kotlang
+
+fun main() {
+    // Var is used when the variable declared is going to be changed - Mutable
+    // Val is used when the variable declared is final or constant - Immutable
+    val name: String;
+    val age: Int
+    val money: Long
+
+    money = 1000000000000000000;
+
+    val pi = 3.14f;
+
+    name = "Kotlin"
+    age = 30
+    print("Hello my name is $name and am $age years old");
+}
