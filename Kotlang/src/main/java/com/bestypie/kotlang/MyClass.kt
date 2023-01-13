d583ebab843fbe24a9b07c792bc0216160375ad1
Karumaidoi <mainahmwangi12@gmail.com>
2023-01-13 21:56:25 +0300

    conditional statements

--- Kotlang/src/main/java/com/bestypie/kotlang/MyClass.kt
+++ Kotlang/src/main/java/com/bestypie/kotlang/MyClass.kt
@@ -3,15 +3,25 @@ package com.bestypie.kotlang
 fun main() {
     // Var is used when the variable declared is going to be changed - Mutable
     // Val is used when the variable declared is final or constant - Immutable
-    val name: String;
-    val age: Int
-    val money: Long
+    val money: Int;
 
-    money = 1000000000000000000;
+    money = 200;
 
-    val pi = 3.14f;
+    // If else statements
+//    if(money >= 100) {
+//        print("You are so much Rich")
+//    } else {
+//        print("Wake up look for Money Alex")
+//    }
 
-    name = "Kotlin"
-    age = 30
-    print("Hello my name is $name and am $age years old");
+    // When statements
+    when (money) {
+        100 -> print("You are a super man")
+        125 -> print("You are a Bat man")
+        200 -> print("More than a hero")
+        else -> {
+            print("Look for more money")
+        }
+    }
+
 }
